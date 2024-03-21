c6cf3c3dee54c64672303a8bba0fcf4c345935cc
Mikewangzi <Wzj18395512425@outlook.com>
2024-03-21 17:40:41 +0800

    Update LoginServlet.java

--- src/main/java/com/wzj/web/LoginServlet.java
+++ src/main/java/com/wzj/web/LoginServlet.java
@@ -27,18 +27,21 @@ public class LoginServlet extends HttpServlet {
         User user1 = userService.getUser(username,password);
         resp.setContentType("text/html;charset=utf-8");
         PrintWriter writer = resp.getWriter();
+        String jsonString1 = JSON.toJSONString("code:200,meg:注册成功");
+        String jsonString2 = JSON.toJSONString("code:404,meg:登陆失败");
+        String jsonString3 = JSON.toJSONString("code:200,meg:登陆成功");
         if(user1==null){
             User user = new User();
             user.setUsername(username);
             user.setPassword(password);
             boolean flag = userService.register(user);
             if(flag){
-                writer.write("code:200,meg:注册成功");
+                writer.write(jsonString1);
             }else{
-                writer.write("code:404,meg:登陆失败");
+                writer.write(jsonString2);
             }
         }else{
-            writer.write("code:200,meg:登陆成功");
+            writer.write(jsonString3);
         }
     }
 }
